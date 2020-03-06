f3febdf13494aa3319bd37954cde4225703b58e9
Patrick Drechsler <patrick.drechsler@WFUE530548>
2020-03-06 14:05:47 +0100

    proxy: add mapping profiles

---
+++ Proxy/Proxy/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Proxy.Controllers.Resources;
+using Proxy.Core.Models;
+
+namespace Proxy.Mapping
+{
+    public class ResourceToModelProfile : Profile
+    {
+        public ResourceToModelProfile()
+        {
+            CreateMap<UserCredentialsResource, User>();
+        }
+    }
+}
