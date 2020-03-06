c480efa14766e19ffee020b618d0a4598306d993
Patrick Drechsler <patrick.drechsler@WFUE530548>
2020-03-06 14:01:40 +0100

    proxy: add persistence

---
+++ Proxy/Proxy/Persistence/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Proxy.Core.Repositories;
+using System.Threading.Tasks;
+
+namespace Proxy.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
