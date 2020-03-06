9c0486851f96b97729e2bc9f95aaeaa02bb0066e
Patrick Drechsler <patrick.drechsler@WFUE530548>
2020-03-06 12:52:37 +0100

    initial commit

---
+++ Client/Client/Controllers/BestellungController.cs
@@ -0,0 +1,33 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BestellungController : ControllerBase
+    {
+        //public void GetBestellStatus(int idf, int bestellId)
+        //{
+        //}
+
+        /// <summary>
+        /// Bestellung aufgeben
+        /// </summary>
+        /// <param name="bestellungAufgegeben"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> BestellungAufgegebenAsync([FromBody] BestellungAufgegeben bestellungAufgegeben) 
+        {
+            var result = await Task.Run(() => 
+            {
+                Thread.Sleep(10);
+                return true;
+            });
+
+            return Ok("ok");
+        }
+    }
+}
